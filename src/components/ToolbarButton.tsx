4a3bceff5c3efbc6fdd114a0b58996662066047f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-11-13 16:38:43 +0000

    WIP

--- src/components/ToolbarButton.tsx
+++ src/components/ToolbarButton.tsx
@@ -1,9 +1,27 @@
-import React, { ButtonHTMLAttributes, HTMLAttributes } from 'react'
+import React, { ButtonHTMLAttributes } from 'react'
 import classNames from 'classnames'
 
-const ToolbarButton: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isActive?: boolean
+}
+
+const ToolbarButton: React.FC<Props> = ({
   className,
+  isActive = false,
   ...props
-}) => <button {...props} className={classNames(className)} />
+}) => {
+  const defaultClasses = 'border px-2 py-1 border-black mr-3'
+
+  return (
+    <button
+      {...props}
+      className={classNames(
+        { 'bg-black text-white': isActive },
+        defaultClasses,
+        className
+      )}
+    />
+  )
+}
 
 export default ToolbarButton
--- src/routes/BasicExample.tsx
+++ src/routes/BasicExample.tsx
@@ -35,17 +35,12 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
     return null
   }
 
-  const buttonClasses = 'border px-2 py-1 border-black mr-3'
-
   return (
     <nav className="bg-gray-200 p-2 mb-4">
       <ToolbarButton
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={classNames(
-          buttonClasses,
-          editor.isActive('bold') ? 'is-active' : ''
-        )}
+        isActive={editor.isActive('bold')}
       >
         bold
       </ToolbarButton>
@@ -53,10 +48,7 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
       <ToolbarButton
         onClick={() => editor.chain().focus().toggleItalic().run()}
         disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={classNames(
-          buttonClasses,
-          editor.isActive('italic') ? 'is-active' : ''
-        )}
+        isActive={editor.isActive('italic')}
       >
         italic
       </ToolbarButton>
@@ -64,10 +56,7 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
       <ToolbarButton
         onClick={() => editor.chain().focus().toggleStrike().run()}
         disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={classNames(
-          buttonClasses,
-          editor.isActive('strike') ? 'is-active' : ''
-        )}
+        isActive={editor.isActive('strike')}
       >
         strike
       </ToolbarButton>
