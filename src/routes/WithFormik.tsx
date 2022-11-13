ff42313f36fce15594fbcc43362658daf02cfa1d
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-11-13 17:15:40 +0000

    WIP

--- src/routes/WithFormik.tsx
+++ src/routes/WithFormik.tsx
@@ -3,29 +3,49 @@ import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Dump from '../components/Dump'
 import Toolbar from '../components/Toolbar'
+import { Form, Formik, FormikProps } from 'formik'
+
+interface EditorInput {
+  text?: string
+  html?: string
+}
 
 const WithFormik: React.FC = () => {
+  const initialValues: EditorInput = {
+    text: '',
+    html: '',
+  }
+
   const editor = useEditor({
     extensions: [StarterKit],
-    content: '',
+    content: initialValues.html,
   })
 
-  return (
-    <div>
-      <Toolbar editor={editor} />
-      <EditorContent editor={editor} className="border border-black p-3" />
+  function handleSubmit(values: EditorInput) {
+    console.log({ values })
+  }
 
-      <div className="mt-3">
-        <Dump
-          expanded
-          title="Details"
-          value={{
-            text: editor?.getText(),
-            html: editor?.getHTML(),
-          }}
-        />
-      </div>
-    </div>
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {({ values }: FormikProps<EditorInput>) => (
+        <Form>
+          <Toolbar editor={editor} />
+          <EditorContent editor={editor} className="border border-black p-3" />
+
+          <div className="mt-3">
+            <Dump
+              expanded
+              title="Details"
+              value={{
+                text: editor?.getText(),
+                html: editor?.getHTML(),
+                formValues: values,
+              }}
+            />
+          </div>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
