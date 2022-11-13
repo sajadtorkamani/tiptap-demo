7cd828766f700b3c5957807d082571eacaf72ce7
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-11-13 17:10:32 +0000

    WIP

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import { ROUTES } from '../constants'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import classNames from 'classnames'
 
 interface Props {
   children: React.ReactNode
 }
 
-const Layout: React.FC<Props> = ({ children }) => (
-  <div className="mx-auto max-w-4xl my-6">
-    <nav className="d-flex mb-4">
-      {Object.values(ROUTES).map((route) => (
-        <Link
-          key={route}
-          to={route}
-          className="text-blue-700 underline mb-3 mr-3"
-        >
-          {route}{' '}
-        </Link>
-      ))}
-    </nav>
+const Layout: React.FC<Props> = ({ children }) => {
+  const location = useLocation()
 
-    {children}
-  </div>
-)
+  return (
+    <div className="mx-auto max-w-4xl my-6">
+      <nav className="d-flex mb-4">
+        {Object.values(ROUTES).map((route) => (
+          <Link
+            key={route}
+            to={route}
+            className={classNames(
+              {
+                'text-black hover:cursor-default': location.pathname === route,
+              },
+              'text-blue-700 underline mb-3 mr-3'
+            )}
+          >
+            {route}{' '}
+          </Link>
+        ))}
+      </nav>
+
+      {children}
+    </div>
+  )
+}
 
 export default Layout
